daef441e165fabb26cc3cb1db2ef493c181a98f4
vikash12345 <vikash@backgroundcheck.email>
2017-02-09 16:02:05 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,7 +4,7 @@ require 'scraperwiki/simple_html_dom.php';
 $MyWebsite = 'http://202.61.43.53/cfms-hc-search/cases/search-result?CasesSearch%5BCASENAMECODE%5D=&CasesSearch%5BCASENO%5D=&CasesSearch%5BCASEYEAR%5D=&CasesSearch%5BCIRCUITCODE%5D=&CasesSearch%5BMATTERCODE%5D=&CasesSearch%5BPARTY%5D=&CasesSearch%5BGOVT_AGENCY_CODE%5D=&CasesSearch%5BFIRNO%5D=&CasesSearch%5BFIRYEAR%5D=&CasesSearch%5BPOLICESTATIONCODE%5D=&CasesSearch%5BADVOCATECODE%5D=&CasesSearch%5BisPending%5D=3&page=';
 
 $serialnumb = 0;
-for ($page = 1; $page < 2; $page++) 
+for ($page = 1; $page < 3; $page++) 
      {
         $NewURL = $MyWebsite . $page . '&per-page=15';
         $html = file_get_html($NewURL);
